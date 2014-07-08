c7d054efc11665c7b82abe71686a88b763327921
Juarez Bochi <jbochi@gmail.com>
2014-07-08 10:12:46 -0300

    Rename session and connected variables

--- spec/functional_spec.lua
+++ spec/functional_spec.lua
@@ -4,17 +4,17 @@ local cassandra = require("cassandra")
 
 describe("cassandra", function()
   before_each(function()
-    cql = cassandra.new()
-    cql:set_timeout(1000)
-    ok, err = cql:connect("127.0.0.1", 9042)
+    session = cassandra.new()
+    session:set_timeout(1000)
+    connected, err = session:connect("127.0.0.1", 9042)
   end)
 
   it("should be possible to connect", function()
-    assert.truthy(ok)
+    assert.truthy(connected)
   end)
 
   it("should be queryble", function()
-    local rows, err = cql:execute("select cql_version, native_protocol_version, release_version from system.local");
+    local rows, err = session:execute("select cql_version, native_protocol_version, release_version from system.local");
     assert.same(1, #rows)
     assert.same(rows[1].native_protocol_version, "2")
   end)
